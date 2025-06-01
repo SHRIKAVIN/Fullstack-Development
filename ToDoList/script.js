68ce809dafd01cc305f22f4762d02627f5e6794a
SHRIKAVIN B <shrikavinb@SHRIKAVINs-MacBook-Air.local>
2025-06-01 23:27:58 +0530

    todo

---
+++ ToDoList/script.js
@@ -0,0 +1,17 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value.trim();
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  // Remove task on click
+  li.onclick = function () {
+    li.remove();
+  };
+
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
